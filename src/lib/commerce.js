4ddb6b423b8cec64e0bbba8dfd01a4b15d20cff4
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-11-09 11:28:00 -0700

    using sandbox api key

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,5 +1,5 @@
 import Commerce from "@chec/commerce.js";
 
 export const commerce = new Commerce(
-  "pk_481500a0ba04e92a6462c570a777d497eca83853d40b1"
+  "pk_test_4815093ed5596559d17ec44b3446578f6fcfb282812c2"
 );
