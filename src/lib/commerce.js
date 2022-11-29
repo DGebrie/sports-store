4b076cd5182557491db4d20af29f4d47e71b1713
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-11-28 18:15:29 -0700

    update API key

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,5 +1,3 @@
 import Commerce from "@chec/commerce.js";
 
-export const commerce = new Commerce(
-  "pk_test_4815093ed5596559d17ec44b3446578f6fcfb282812c2"
-);
+export const commerce = new Commerce(process.env.REACT_APP_CHECK_PUBLIC_KEY);
